cad7001acf2774a83b9dbf0d63c838b369be77d6
Oscar Luna <CaporalesLuna@gmail.com>
2020-03-17 12:46:59 -0700

    added directory and menu components with styling hover and overflow effects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component.jsx';
 
 function App() {
-  return <div><HomePage/></div>
+  return <div>
+    <HomePage/>
+    </div>
 };
 
 export default App;
