2d43022efaa8af6f150966924c2550c305fd7f8e
Oscar Luna <CaporalesLuna@gmail.com>
2020-03-20 12:19:23 -0700

    added addItem and removeItem functionality to checkout item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
 import {
   selectCartItems,
   selectCartTotal
 } from "../../redux/cart/cart.selectors";
-
-import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 import "./checkout.styles.scss";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
     <div className="checkout-header">
+      <div className="header-block">
+        <span>Product</span>
+      </div>
       <div className="header-block">
         <span>Description</span>
       </div>
@@ -22,16 +26,14 @@ const CheckoutPage = ({ cartItems, total }) => (
       <div className="header-block">
         <span>Price</span>
       </div>
-      <div className="header-block">
+    </div>
+    <div className="header-block">
         <span>Remove</span>
       </div>
-    </div>
     {cartItems.map(cartItem => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">
-      <span>TOTAL: ${total}</span>
-    </div>
+    <div className="total">TOTAL: ${total}</div>
   </div>
 );
 
