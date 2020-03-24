cf816e7fb2ea2786be76a1f5e1ac5da46f1672cd
Oscar Luna <CaporalesLuna@gmail.com>
2020-03-24 13:42:50 -0700

    replaced SCSS files with Styled Components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import './cart-item.styles.scss';
+import { CartItemContainer, CartItemDetails, CartItemName, CartItemImage } from './cart-item.styles'
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-  <div className='cart-item'>
-    <img src={imageUrl} alt='item' />
-    <div className='item-details'>
-      <span className='name'>{name}</span>
+  <CartItemContainer>
+    <CartItemImage src={imageUrl} alt='item' />
+    <CartItemDetails>
+      <CartItemName>{name}</CartItemName>
       <span className='price'>
         {quantity} * ${price}
       </span>
-    </div>
-  </div>
+    </CartItemDetails>
+  </CartItemContainer>
 );
 
 export default CartItem;
