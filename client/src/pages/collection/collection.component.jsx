b399a510ffd019a32580e87a5bd05a6330957063
Oscar Luna <oscarluna.webdev@gmail.com>
2026-06-14 19:59:54 -0700

    Update collection.component.jsx

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -22,7 +22,7 @@ const CollectionPage = ({ collection,ItemPage, match }) => {
         ))}
       </CollectionItemsContainer>
        <Route
-          path={`${match.path}/:collectionId/:${items.item.id}`} component={CollectionPage} />
+          path={`${match.path}/:${item.id}`} component={CollectionPage} />
     </CollectionPageContainer>
   );
 };
