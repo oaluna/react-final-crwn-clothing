d39c2ab4a78757de646bb5f37450e07873f9b5ee
Oscar Luna <oscarluna.webdev@gmail.com>
2026-06-15 02:23:41 +0000

    removed default exports from store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -10,8 +10,7 @@ if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-const persistor = persistStore(store);
+export const persistor = persistStore(store);
 
-export default { store, persistor };
